bbfa30ada3a72ffeff14f4419c934f4c0e0d02be
Raj Movva <rmovva@mit.edu>
2020-06-24 07:41:13 +0000

    [minor] removed return statement in train_lth main(); added lth train bash file

--- fairseq_cli/train_lth.py
+++ fairseq_cli/train_lth.py
@@ -70,7 +70,6 @@ def main(args, init_distributed=False):
     # Build model and criterion
     model = task.build_model(args)
     criterion = task.build_criterion(args)
-    return;
     logger.info(model)
     logger.info('model {}, criterion {}'.format(args.arch, criterion.__class__.__name__))
     logger.info('num. model params: {} (num. trained: {})'.format(
